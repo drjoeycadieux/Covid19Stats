9064d7f94b5d02a0c8bc47570768c9c5c177d69a
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-03-05 15:26:18 -0500

    Deploy some new code! <header></header>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./styles.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Carousel from "./components/Carousel";
 import Features from "./views/Features";
+import Home from "./views/Home";
 import Announcement from "./components/Announcement";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       <br />
-      <Carousel />
-
       <Announcement />
 
       <h1> Covid-19 Stats</h1>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,42 @@
 import React from "react";
-import "./styles.css";
 
 function Home() {
   return (
     <div>
-      <h1> Home </h1>
+      <header>
+        <div class="w-full bg-gray-800 md:pt-20 md:pb-20 text-white">
+          <div class="lg:max-w-5xl mx-auto md:px-4 p-4">
+            <h1 class="text-4xl font-bold">Canada COVID-19 Tracker</h1>
+            <h4 class="text-xs">
+              Source:
+              <a
+                class="border-b border-dotted text-blue-600 border-blue-600 hover:text-blue-800 hover:border-blue-800"
+                target="_blank"
+                href="https://www.canada.ca/en/public-health/services/diseases/coronavirus-disease-covid-19.html"
+              >
+                Canada Deptartment of Health
+              </a>
+              &middot; Last updated 3/05/2021 3:30 p.m.
+            </h4>
+
+            <br />
+
+            <div class="mt-10 p-4 bg-red-600 rounded-lg shadow-md text-white text-left">
+              <v-alert
+                border="bottom"
+                colored-border
+                type="warning"
+                elevation="2"
+              >
+                Attention New health instructions and curfew in force throughout
+                Québec. Avoid travel and consult the measures that apply in your
+                region to learn the details. You can also consult all the
+                information on COVID‑19.
+              </v-alert>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
