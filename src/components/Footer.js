2a5d69bf76809a53e4c6c832df305b05c4ddf724
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-03-06 16:39:25 -0500

    fix some error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <p> COVID-19 TRACKER </p>
+    <div class="pb-4">
+      <h4 class="text-xs">
+        Developed by Joey Cadieux &middot;{" "}
+        <a class="text-blue-600" href="mailto:joeycadieux161@gmail.com">
+          Contact Me
+        </a>
+        &middot;
+        <a class="text-blue-600" href="https://forms.gle/Hqve4wC3xmdruhhJA">
+          Found an issue?
+        </a>
+      </h4>
     </div>
   );
 };
